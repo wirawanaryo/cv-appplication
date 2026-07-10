b5ef9a4c250c74c7806a2c4395b70a0bab5baf55
unknown <wirawanaryo99@gmail.com>
2026-07-10 22:13:32 +0700

    add button done?

--- src/components/forms/WorkExpForm.tsx
+++ src/components/forms/WorkExpForm.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 import styles from "./WorkExpForm.module.css";
 
 interface WorkInfoType {
-  id : string;
+  id: string;
   company: string;
   position: string;
   startdate: string;
@@ -14,11 +14,17 @@ interface WEformProps {
   WEData: WorkInfoType[];
   UpdateWEData: React.Dispatch<React.SetStateAction<WorkInfoType[]>>;
 }
+interface MainformProps {
+  WEData: WorkInfoType[];
+  UpdateWEData: React.Dispatch<React.SetStateAction<WorkInfoType[]>>;
+  data: WorkInfoType;
+  index: number;
+}
 
-function MainForm({ WEData, UpdateWEData }: WEformProps) {
+function MainForm({ WEData, UpdateWEData, data, index }: MainformProps) {
   return <form className={styles.MainForm} id="WEForm">
     <div className={styles.FormHeader}>
-      <h2>Company 1</h2>
+      <h2>Company {index+1}</h2>
       <button type="button" className={styles.DeleteButton}>🗑</button>
     </div>
     <div className={styles.FormInputHolders}>
@@ -27,6 +33,7 @@ function MainForm({ WEData, UpdateWEData }: WEformProps) {
         type="text"
         id={styles.Company}
         name="Company"
+        defaultValue={data.company}
         placeholder="Big Bad Corp"
       />
     </div>
@@ -36,6 +43,7 @@ function MainForm({ WEData, UpdateWEData }: WEformProps) {
         type="text"
         id={styles.Position}
         name="Position"
+        defaultValue={data.position}
         placeholder="Manager"
       />
     </div>
@@ -45,6 +53,7 @@ function MainForm({ WEData, UpdateWEData }: WEformProps) {
         type="date"
         id={styles.StartDate}
         name="StartDate"
+        defaultValue={data.startdate}
       />
     </div>
     <div className={styles.FormInputHolders}>
@@ -53,6 +62,7 @@ function MainForm({ WEData, UpdateWEData }: WEformProps) {
         type="date"
         id={styles.EndDate}
         name="EndDate"
+        defaultValue={data.enddate}
       />
     </div>
     <div className={styles.FormAreaHolders}>
@@ -61,6 +71,7 @@ function MainForm({ WEData, UpdateWEData }: WEformProps) {
         id={styles.summary}
         name="summary"
         rows={3}
+        defaultValue={data.summary}
         placeholder="Tell us about it..."
       ></textarea>
     </div>
@@ -68,25 +79,43 @@ function MainForm({ WEData, UpdateWEData }: WEformProps) {
   </form>
 }
 
+function MainFormContainer({ WEData, UpdateWEData }: WEformProps) {
+  return <div id="WEformContainer">
+    {WEData.map((WE, index) => (
+      <MainForm key={WE.id} WEData={WEData} UpdateWEData={UpdateWEData} data={WE} index={index} />
+    ))}
+  </div>
+}
+
 export function WorkExpForm({ WEData, UpdateWEData }: WEformProps) {
   function handleAddMore() {
-    WEData.push({
-      id: crypto.randomUUID(),
-      company: '',
-      position: '',
-      startdate: '',
-      enddate: '',
-      summary: ''
-    });
-    console.log('Add more button clicked')
-    console.log(WEData)
+    UpdateWEData(prev => [
+      ...prev, {
+        id: crypto.randomUUID(),
+        company: '',
+        position: '',
+        startdate: '',
+        enddate: '',
+        summary: ''
+      },
+    ])
+    // WEData.push({
+    //   id: crypto.randomUUID(),
+    //   company: '',
+    //   position: '',
+    //   startdate: '',
+    //   enddate: '',
+    //   summary: ''
+    // });
+    // console.log('Add more button clicked')
+    // console.log(WEData)
   }
-  
+
 
   return <div className={styles.WorkExpForm}>
     <h2>Work Experience</h2>
     <hr></hr>
-    <MainForm WEData={WEData} UpdateWEData={UpdateWEData} />
+    <MainFormContainer WEData={WEData} UpdateWEData={UpdateWEData} />
     <div className={styles.formButtons}>
       <button type="submit" form="WEForm">Update Data</button>
       <button type="button" onClick={handleAddMore}>Add More</button>
